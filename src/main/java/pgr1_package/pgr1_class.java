13ec5f65774c77d0abbefb6f6f1af1468a916d94
nikhiy <nikhilchowdary2006@gmail.com>
2026-04-26 10:34:09 +0530

    added src

---
+++ src/main/java/pgr1_package/pgr1_class.java
@@ -0,0 +1,12 @@
+package pgr1_package;
+
+public class pgr1_class {
+	public static String display(int m) {
+		if(m<0 || m>100) return "Invalid";
+		else if(m<40) return "fail";
+		return "pass";
+	}
+	public static void main(String[] args) {
+		System.out.println(display(50));
+	}
+}
