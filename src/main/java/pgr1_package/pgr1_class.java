6b90ebf398cc52c8e8218d537e047e650491770a
nikhiy <nikhilchowdary2006@gmail.com>
2026-04-26 12:35:58 +0530

    fixed case issue

--- src/main/java/pgr1_package/pgr1_class.java
+++ src/main/java/pgr1_package/pgr1_class.java
@@ -1,12 +1,13 @@
 package pgr1_package;
 
 public class pgr1_class {
-	public static String display(int m) {
-		if(m<0 || m>100) return "Invalid";
-		else if(m<40) return "fail";
-		return "pass";
-	}
-	public static void main(String[] args) {
-		System.out.println(display(50));
-	}
+    public static String display(int m) {
+        if(m < 0 || m > 100) return "invalid";  // ✅ FIXED
+        else if(m < 40) return "fail";
+        return "pass";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(display(50));
+    }
 }
